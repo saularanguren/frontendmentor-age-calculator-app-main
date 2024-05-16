f4f1cc39635f2fa411f2cfac178643f0a9f32dee
Saul Gabriel Aranguren <gabomoralejo@gmail.com>
2024-05-16 16:49:10 -0400

    Failed image upload and package manager update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './src/css/style.css'
 import cardElementHtml from './src/app/pages/card.html?raw'
+import iconArrow from './src/assets/images/icon-arrow.svg'
 import { functionRenderElementsHtml } from './src/app/use-cases/renderElementsHtml'
 import { clearInputsText } from './src/app/use-cases/clearCompleted'
 import { clickInButton } from './src/app/use-cases/clickButton'
@@ -8,6 +9,7 @@ import { functionPresentatioProyect } from './src/app/use-cases/presentation'
 
 //* principal function *
 functionRenderElementsHtml('#app', cardElementHtml)
+document.querySelector('img').src = `${iconArrow}`
 document.addEventListener("DOMContentLoaded", functionPresentatioProyect);
 
 //* secondary functions *
